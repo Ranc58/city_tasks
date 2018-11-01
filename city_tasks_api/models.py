d6f3dbbe9e65edda90e4ba57e0fce7e740a4fef3
Ranc58 <rancvova@gmail.com>
2018-11-01 22:02:59 +0300

    Add models

--- city_tasks_api/models.py
+++ city_tasks_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_('Customer'))
+    position = models.CharField(max_length=100, verbose_name=_('Present position'))
+    city = models.CharField(max_length=100, verbose_name=_('City'))
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100, verbose_name=_('Task title'))
+    description = models.TextField(verbose_name=_('Task description'))
+    performer_client = models.ManyToManyField(Client)
+    controller_client = models.ForeignKey(Client, on_delete=models.DO_NOTHING) #todo change on_delete
+
+    def __str__(self):
+        return self.title
